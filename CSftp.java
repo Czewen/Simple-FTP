6a2b83b9781e0ae170baca2738daef7c66586a83
Cze Wen Goh <czewengoh@Hotmail.com>
2015-02-01 03:09:10 -0800

    Implemented user input checking

--- CSftp.java
+++ CSftp.java
@@ -14,18 +14,59 @@ public class CSftp
 
     public static void main(String [] args)
     {
-	byte cmdString[] = new byte[MAX_LEN];
-	try {
-	    for (int len = 1; len > 0;) {
-		System.out.print("csftp> ");
-		len = System.in.read(cmdString);
-		if (len <= 0) 
-		    break;
-		// Start processing the command here.
-		System.out.println("900 Invalid command.");
-	    }
-	} catch (IOException exception) {
-	    System.err.println("998 Input error while reading commands, terminating.");
+		byte cmdString[] = new byte[MAX_LEN];
+		try {
+		    for (int len = 1; len > 0;) {
+				System.out.print("csftp> ");
+				len = System.in.read(cmdString);
+				if (len <= 0) 
+				    break;
+				// Start processing the command here.
+				String cmd = readableText(cmdString, 0, 255);
+				if(!checkCmdString(cmd)){
+					System.out.println("800 Invalid command.");
+					break;
+				}
+					
+		   }
+		} 
+		catch (IOException exception) {
+		    System.err.println("998 Input error while reading commands, terminating.");
+		}
 	}
+    
+    public static boolean checkCmdString(String cmdString){
+    	boolean correct = false;
+    	
+    	String[] accepted_inputs = {"open", "user", "close", "quit", "get", "put", "cd", "dir"};
+    	for(int i=0; i<accepted_inputs.length; i++){
+    		if(cmdString.equals(accepted_inputs[i])){
+    			correct = true;
+    			System.out.println("match");
+    		}
+    	}
+    	return correct;
     }
+    
+    public static boolean isPrintableAscii(byte value)
+    {
+        return (value >= 32 ) && (value < 127);
+    }
+
+    public static String readableText(byte[] buffer, int offset, int bufferSize)
+    {
+        StringBuilder builder = new StringBuilder();
+        for( int index = 0; index < bufferSize; ++index)
+        {
+            byte current = buffer[offset+index];
+            if( isPrintableAscii(current))
+            {
+                builder.append((char)current);
+            }
+            
+        }
+
+        return builder.toString();
+    }
+    
 }
